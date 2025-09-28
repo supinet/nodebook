81d361e28cb044068bb91efe2ac324553fed5385
fernando.supi@gmail.com <fernando.supi@gmail.com>
2025-09-28 10:09:51 -0400

    start project add swagger

---
+++ README.md
@@ -0,0 +1,6 @@
+npm install express
+npm install nodemon
+npm install swagger-ui-express swagger-jsdoc
+
+npx nodemon app.js
+
---
+++ routes/books.js
@@ -0,0 +1,30 @@
+const { Router } = require('express');
+
+const router = Router();
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Retrieve a list of users
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: string
+ *                   name:
+ *                     type: string
+ */
+router.get('/', (req, res) => {
+	res.send('Books endpoint');
+});
+
+module. exports = router;
+
